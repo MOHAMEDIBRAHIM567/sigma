164269d9c4c61a0cac0cf741894d54fcbe1e0db9
MOHAMEDIBRAHIM567 <78454157+MOHAMEDIBRAHIM567@users.noreply.github.com>
2024-05-17 20:53:01 +0300

    Add files via upload

--- Session4Task/src/test/java/LoginDataProviders.java
+++ Session4Task/src/test/java/LoginDataProviders.java
@@ -12,7 +12,9 @@ public class LoginDataProviders extends BaseTest {
                 {"standard_user","secret_sauce"},
                 {"locked_out_user","secret_sauce"},
                 {"problem_user","secret_sauce"},
-                {"performance_glitch_user","secret_sauce"}
+                {"performance_glitch_user","secret_sauce"},
+                {"invalide","secret_sauce"},
+                {"performance_glitch_user","invalide"}
         };
     }
     @Test(dataProvider = "LoginData")
